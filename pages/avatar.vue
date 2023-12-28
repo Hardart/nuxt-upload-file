84a7d5fe04a49b00b26e02ae81708b68d7f76fc1
Hardart <kaino@bk.ru>
2023-12-29 02:18:46 +0300

    change routes

---
+++ pages/avatar.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+const src = ref('')
+const avatarSrc = ref('')
+const uploadProgress = ref(0)
+const isShowAvatarDrag = ref(false)
+const form = ref<HTMLFormElement | null>(null)
+
+const onSubmit = async () => {
+  if (!form.value) return
+  uploadForm('/api/upload', form.value)
+  form.value.reset()
+}
+
+const onConfirm = async (props: object) => {
+  if (!form.value) return
+
+  avatarSrc.value = await $fetch('/api/crop-image', { body: { props, src: src.value }, method: 'POST' })
+  isShowAvatarDrag.value = false
+}
+
+function uploadForm(url: string, formElement: HTMLFormElement) {
+  const form = new FormData(formElement)
+  const xhr = new XMLHttpRequest()
+  xhr.open('post', url, true)
+  xhr.upload.onprogress = function ({ total, loaded }) {
+    // Upload progress here
+    uploadProgress.value = Math.round((loaded / total) * 100)
+  }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      // Uploaded
+      const fileName = xhr.response
+      if (fileName.trim()) src.value = fileName
+      isShowAvatarDrag.value = true
+    }
+  }
+  xhr.send(form)
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit" ref="form">
+    <input type="text" name="title" />
+    <input type="file" name="image" />
+    <button type="submit" class="">Отправить</button>
+  </form>
+
+  <HdrtDragzone :src="src" v-if="src && isShowAvatarDrag" @confirm="onConfirm" />
+  <div class="w-32 h-32 rounded-full overflow-hidden mx-auto border-4 border-rose-300">
+    <img :src="avatarSrc" class="object-cover w-full" alt="" v-if="avatarSrc" />
+    <div class="bg-rose-200 w-full h-full"></div>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,62 @@
 <script lang="ts" setup>
-const src = ref('')
-const avatarSrc = ref('')
-const uploadProgress = ref(0)
-const isShowAvatarDrag = ref(false)
-const form = ref<HTMLFormElement | null>(null)
+import { useScreenOrientation } from '@vueuse/core'
 
-const onSubmit = async () => {
-  if (!form.value) return
-  uploadForm('/api/upload', form.value)
-  form.value.reset()
+const { isSupported, orientation, angle, lockOrientation, unlockOrientation } = useScreenOrientation()
+
+const { data } = await useFetch('/api/files')
+if (!data.value) throw createError('')
+const itemsState = ref<{ [key: number]: boolean }>({})
+
+for (let i = 0; i < data.value.length / 2; i++) {
+  itemsState.value[i] = false
 }
 
-const onConfirm = async (props: object) => {
-  if (!form.value) return
-
-  avatarSrc.value = await $fetch('/api/crop-image', { body: { props, src: src.value }, method: 'POST' })
-  isShowAvatarDrag.value = false
+function toggleItemState(i: number) {
+  Object.keys(itemsState.value).forEach((key, index) => {
+    itemsState.value[index] = i == index ? !itemsState.value[index] : itemsState.value[index]
+  })
 }
+const mult = data.value?.length / 2
 
-function uploadForm(url: string, formElement: HTMLFormElement) {
-  const form = new FormData(formElement)
-  const xhr = new XMLHttpRequest()
-  xhr.open('post', url, true)
-  xhr.upload.onprogress = function ({ total, loaded }) {
-    // Upload progress here
-    uploadProgress.value = Math.round((loaded / total) * 100)
-  }
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      // Uploaded
-      const fileName = xhr.response
-      if (fileName.trim()) src.value = fileName
-      isShowAvatarDrag.value = true
-    }
-  }
-  xhr.send(form)
-}
+const isHorisontal = computed(() => angle)
 </script>
 
 <template>
-  <form @submit.prevent="onSubmit" ref="form">
-    <input type="text" name="title" />
-    <input type="file" name="image" />
-    <button type="submit" class="">Отправить</button>
-  </form>
-
-  <HdrtDragzone :src="src" v-if="src && isShowAvatarDrag" @confirm="onConfirm" />
-  <div class="w-32 h-32 rounded-full overflow-hidden mx-auto border-4 border-rose-300">
-    <img :src="avatarSrc" class="object-cover w-full" alt="" v-if="avatarSrc" />
-    <div class="bg-rose-200 w-full h-full"></div>
+  <div class="space-y-52">
+    <div
+      class="wrap h-screen flex justify-center"
+      :class="[itemsState[index] && 'rotat']"
+      v-for="(imageSrc, index) in data?.slice(0, mult)"
+      @dblclick="toggleItemState(index)"
+    >
+      <div class="wrap__container h-full">
+        <img :src="imageSrc" class="object-contain h-full" alt="" loading="lazy" />
+      </div>
+      <div class="wrap_reverse">
+        <img :src="data![index + mult]" loading="lazy" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrap.rotat {
+  transform: rotateY(-180deg);
+}
+.wrap {
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.8s;
+}
+
+.wrap__container {
+  position: relative;
+  backface-visibility: hidden;
+}
+
+.wrap_reverse {
+  position: absolute;
+  transform: rotateY(180deg);
+  backface-visibility: hidden;
+}
+</style>
