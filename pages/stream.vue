fc37fd947f2e42b8bc687d761d1566663ff4cd2f
Hardart <kaino@bk.ru>
2024-02-04 23:02:55 +0300

    some change

--- pages/stream.vue
+++ pages/stream.vue
@@ -1,7 +1,30 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface ITrackData {
+  artistName: string
+  trackTitle: string
+  covers: {
+    art30: string
+    art60: string
+    art100: string
+  }
+}
+const { $ws } = useNuxtApp()
+onMounted(initSocket)
+const trackData = ref<ITrackData | null>()
+function initSocket() {
+  const socket = $ws('admin', '3068')
+  socket.on('radio:track', (data: ITrackData) => (trackData.value = data))
+}
+</script>
 
 <template>
-  <HdStream />
+  <div class="container my-32 mx-auto">
+    <h1 class="text-7xl">Stream Page</h1>
+    <div v-if="trackData">
+      <h3>{{ trackData.artistName }}</h3>
+      <img :src="trackData.covers?.art100" alt="" />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
